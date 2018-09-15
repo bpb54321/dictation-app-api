46f43f33caf8c84f6255bdac3c86d704ebe4b357
Brian Blosser <bpb54321@gmail.com>
2018-09-15 17:01:13 -0400

    Made some changes to the models and admin

--- rest_api/admin.py
+++ rest_api/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import MediaItem, Clip
 
-admin.site.register(MediaItem)
-admin.site.register(Clip)
+
+class ClipInline(admin.StackedInline):
+    model = Clip
+    extra = 3
+
+
+class MediaItemAdmin(admin.ModelAdmin):
+    inlines = [ClipInline]
+
+admin.site.register(MediaItem, MediaItemAdmin)
--- rest_api/models.py
+++ rest_api/models.py
@@ -2,19 +2,27 @@ from django.db import models
 
 
 class MediaItem(models.Model):
+    title = models.CharField(max_length=200, default='')
     type = models.TextField(
         choices=(
             ('youtube', 'Youtube Video'),
             ('audio', 'Audio'),
-        )
+        ),
+        default='youtube'
     )
-    url = models.URLField()
-    youtube_id = models.CharField(max_length=200)
+    url = models.URLField(default='', blank=True)
+    youtube_id = models.CharField(max_length=200, default='', blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Clip(models.Model):
     transcription = models.TextField(default='')
     translation = models.TextField(default='')
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_time = models.CharField(max_length=5, default='00:00')
+    end_time = models.CharField(max_length=5, default='00:00')
     media_item = models.ForeignKey(MediaItem, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{start_time} - {end_time}'
